4b5e148141c9bcc670d2153e5c8f2f6502cfeb47
John Rees <john@bitsushi.com>
2021-04-30 23:50:07 +0100

    initial commit, test connection to each serum market
    related #1

---
+++ serum-connection-test/README.md
@@ -0,0 +1,40 @@
+```
+❯ npx esr src/index.ts
+
+1 BTC : 40000 USDC
+connection successful
+1 BTC : 45000 USDC
+connection successful
+1 BTC : 50000 USDC
+connection successful
+1 BTC : 55000 USDC
+connection successful
+1 BTC : 60000 USDC
+connection successful
+1 BTC : 65000 USDC
+connection successful
+1 BTC : 70000 USDC
+connection successful
+1 BTC : 75000 USDC
+connection successful
+1 BTC : 80000 USDC
+connection successful
+40000 USDC : 1 BTC
+connection successful
+45000 USDC : 1 BTC
+connection successful
+50000 USDC : 1 BTC
+connection successful
+55000 USDC : 1 BTC
+connection successful
+60000 USDC : 1 BTC
+connection successful
+65000 USDC : 1 BTC
+connection successful
+70000 USDC : 1 BTC
+connection successful
+75000 USDC : 1 BTC
+connection successful
+80000 USDC : 1 BTC
+connection successful
+```
---
+++ serum-connection-test/src/index.ts
@@ -0,0 +1,59 @@
+require("dotenv").config("..");
+
+import { Market } from "@project-serum/serum";
+import { Connection, PublicKey } from "@solana/web3.js";
+import data from "./devnet_beta_market_data.json";
+
+const connection = new Connection(process.env.RPC_URL);
+const programId = new PublicKey(process.env.DEX_PROGRAM_ID);
+
+(async function () {
+  // sort data into calls and put arrays sorted in ascending order
+  const { calls, puts } = sortData(data);
+
+  for (const market of [...calls, ...puts]) {
+    const {
+      quoteAssetPerContract,
+      quoteAssetSymbol,
+      serumMarketAddress,
+      underlyingAssetPerContract,
+      underlyingAssetSymbol,
+    } = market;
+
+    const marketAddress = new PublicKey(serumMarketAddress);
+
+    console.log(
+      [
+        `${underlyingAssetPerContract} ${underlyingAssetSymbol}`,
+        `${quoteAssetPerContract} ${quoteAssetSymbol}`,
+      ].join(" : ")
+    );
+
+    try {
+      // attempt to connect to the serum market
+      await Market.load(connection, marketAddress, undefined, programId);
+      console.log("connection successful");
+    } catch (err) {
+      console.error({ error: err });
+    }
+  }
+})();
+
+function sortData(data) {
+  return data.reduce(
+    (acc, curr) => {
+      const type = curr.underlyingAssetSymbol === "BTC" ? "calls" : "puts";
+      acc[type] = acc[type].concat(curr).sort(lowestToHighest);
+      return acc;
+    },
+    { calls: [], puts: [] }
+  );
+
+  function lowestToHighest(a, b) {
+    return (
+      Number(a.quoteAssetPerContract) +
+      Number(a.underlyingAssetPerContract) -
+      (Number(b.quoteAssetPerContract) + Number(b.underlyingAssetPerContract))
+    );
+  }
+}
