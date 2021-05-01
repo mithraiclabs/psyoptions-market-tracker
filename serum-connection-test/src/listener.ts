ded6d17bca2628949f275d47957e20a2988eb81b
John Rees <john@bitsushi.com>
2021-05-01 22:06:38 +0100

    feat: push serum events into timescaledb

--- serum-connection-test/src/listener.ts
+++ serum-connection-test/src/listener.ts
@@ -1,16 +1,37 @@
+import axios from "axios";
 import WebSocket from "ws";
 import markets from "./data/markets.json";
 
 const ws = new WebSocket("ws://localhost:8000/v1/ws");
 
-ws.onmessage = (message) => {
-  console.log(JSON.parse(message.data));
+ws.onmessage = async (message) => {
+  try {
+    const { timestamp, ...data } = JSON.parse(message.data);
+
+    await axios.post("http://localhost:8080/v1/graphql", {
+      query: `
+          mutation InsertEvent($data: jsonb, $timestamp: timestamptz) {
+            insert_serum_vial_events_one(object: {data: $data, timestamp: $timestamp}) {
+              timestamp
+            }
+          }
+        `,
+      variables: {
+        data,
+        timestamp,
+      },
+    });
+
+    console.log({ timestamp, data });
+  } catch (err) {
+    console.error({ err });
+  }
 };
 
 ws.onopen = () => {
   // https://github.com/tardis-dev/serum-vial#supported-channels--corresponding-message-types
   const channels = [
-    // "trades",
+    "trades", // unused?
     "level1", // for 'quote'
     "level2", // for 'l2update'
     "level3", // for 'open' and 'done'
