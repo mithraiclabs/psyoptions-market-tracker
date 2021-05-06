853af30f54c4901925ad7a1ba9fd3182a5ab175c
John Rees <john@bitsushi.com>
2021-05-06 10:40:21 +0100

    checking for trades without vial

--- serum-connection-test/src/testing-trades.ts
+++ serum-connection-test/src/testing-trades.ts
@@ -5,7 +5,6 @@ import data from "./data/devnet_beta_market_data.json";
 import { sortData } from "./utils";
 
 const connection = new Connection(process.env.RPC_URL);
-const programId = new PublicKey(process.env.PROGRAM_ID);
 const dexProgramId = new PublicKey(process.env.DEX_PROGRAM_ID);
 
 async function go() {
@@ -15,19 +14,11 @@ async function go() {
   const { calls, puts } = sortData(data);
 
   for (const market of [...calls, ...puts]) {
-    const {
-      quoteAssetPerContract,
-      quoteAssetSymbol,
-      serumMarketAddress,
-      underlyingAssetPerContract,
-      underlyingAssetSymbol,
-    } = market;
+    const { serumMarketAddress } = market;
 
     const marketAddress = new PublicKey(serumMarketAddress);
-    // const marketAddress = new PublicKey(market.optionMarketAddress);
 
     try {
-      // attempt to connect to the serum market
       const market = await Market.load(
         connection,
         marketAddress,
@@ -41,20 +32,14 @@ async function go() {
           title: serumMarketAddress,
           message: JSON.stringify(fills),
         });
-
-        console.log(
-          [
-            `${underlyingAssetPerContract} ${underlyingAssetSymbol}`,
-            `${quoteAssetPerContract} ${quoteAssetSymbol}`,
-          ].join(" : "),
-          fills
-        );
       }
     } catch (err) {
       console.error({ error: err });
     }
   }
-  setTimeout(go, 60000);
+
+  // check for new trades every 20 - 60 seconds
+  setTimeout(go, Math.max(20, Math.random() * 60) * 1000);
 }
 
 go();
