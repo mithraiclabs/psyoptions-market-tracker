e51b97ba0f3beed09ee0bbd73b5c0263ca840311
John Rees <john@bitsushi.com>
2021-05-08 15:08:38 +0100

    fix: typo in event_types migration

--- hasura/migrations/1620473248814_add_serum_vial_event_types/up.sql
+++ hasura/migrations/1620473248814_add_serum_vial_event_types/up.sql
@@ -3,7 +3,7 @@ INSERT INTO serum_vial_event_types (value, comment) VALUES
 ('trade', 'https://github.com/tardis-dev/serum-vial#trade'),
 ('quote', 'https://github.com/tardis-dev/serum-vial#quote'),
 ('l2snapshot', 'https://github.com/tardis-dev/serum-vial#l2snapshot'),
-('l2update', 'https://github.com/tardis-dev/serum-vial#l2snapshot'),
+('l2update', 'https://github.com/tardis-dev/serum-vial#l2update'),
 ('l3snapshot', 'https://github.com/tardis-dev/serum-vial#l3snapshot'),
 ('open', 'https://github.com/tardis-dev/serum-vial#open'),
 ('fill', 'https://github.com/tardis-dev/serum-vial#fill'),
