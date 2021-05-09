7fd9f040859a584f3505794db40ab4e9a833be17
John Rees <john@bitsushi.com>
2021-05-09 21:21:20 +0100

    fix(hasura): fix enum table migrations

--- hasura/migrations/1620473055295_create_table_public_serum_vial_event_types/up.sql
+++ hasura/migrations/1620473055295_create_table_public_serum_vial_event_types/up.sql
@@ -1,1 +1,13 @@
 CREATE TABLE "public"."serum_vial_event_types"("value" text NOT NULL, "comment" text NOT NULL, PRIMARY KEY ("value") );
+
+INSERT INTO serum_vial_event_types (value, comment) VALUES
+('recent_trades', 'https://github.com/tardis-dev/serum-vial#recent_trades'),
+('trade', 'https://github.com/tardis-dev/serum-vial#trade'),
+('quote', 'https://github.com/tardis-dev/serum-vial#quote'),
+('l2snapshot', 'https://github.com/tardis-dev/serum-vial#l2snapshot'),
+('l2update', 'https://github.com/tardis-dev/serum-vial#l2update'),
+('l3snapshot', 'https://github.com/tardis-dev/serum-vial#l3snapshot'),
+('open', 'https://github.com/tardis-dev/serum-vial#open'),
+('fill', 'https://github.com/tardis-dev/serum-vial#fill'),
+('change', 'https://github.com/tardis-dev/serum-vial#change'),
+('done', 'https://github.com/tardis-dev/serum-vial#done');
--- hasura/migrations/1620484137567_create_table_public_serum_clusters/up.sql
+++ hasura/migrations/1620484137567_create_table_public_serum_clusters/up.sql
@@ -1,1 +1,3 @@
 CREATE TABLE "public"."serum_clusters"("value" text NOT NULL, "url" text, PRIMARY KEY ("value") );
+
+INSERT INTO "public"."serum_clusters"("value") VALUES('devnet');
