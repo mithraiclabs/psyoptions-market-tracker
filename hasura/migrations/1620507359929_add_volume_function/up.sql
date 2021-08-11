db02012e27931fcc5b699affe33c6bedd2f8f96d
Tommy Johnson <tommyjohnson1028@gmail.com>
2021-08-11 14:35:56 -0700

    fix change and volume functions

--- hasura/migrations/1620507359929_add_volume_function/up.sql
+++ hasura/migrations/1620507359929_add_volume_function/up.sql
@@ -1,5 +1,5 @@
 CREATE FUNCTION volume(market_row serum_markets)
 RETURNS NUMERIC AS $$
-  SELECT SUM(serum_events.price * serum_events.size) FROM serum_events
+  SELECT SUM(serum_events.size) FROM serum_events
   WHERE serum_events.serum_market_address = market_row.address AND serum_events.type = 'trade'
 $$ LANGUAGE sql STABLE;
--- hasura/migrations/1620514653223_add_change_function/up.sql
+++ hasura/migrations/1620514653223_add_change_function/up.sql
@@ -2,7 +2,7 @@ CREATE FUNCTION change(market_row serum_markets, duration text = '24 hours', per
 RETURNS NUMERIC AS $$
   select
   CASE WHEN percentage THEN
-    (price/(select price from serum_events where timestamp < now() - '1 hour'::interval and serum_events.type = 'trade' ORDER BY serum_events.timestamp desc LIMIT 1) * 100)
+    (price/(select price from serum_events where timestamp < now() - duration::interval and serum_events.type = 'trade' ORDER BY serum_events.timestamp desc LIMIT 1) * 100)
   ELSE
     (price - (select price from serum_events where timestamp < now() - duration::interval and serum_events.type = 'trade' ORDER BY serum_events.timestamp desc LIMIT 1))
   END
