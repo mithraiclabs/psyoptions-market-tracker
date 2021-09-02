09ac7047d97475b5a3b0fcc76b524cb2cc88246f
Tommy Johnson <tommyjohnson1028@gmail.com>
2021-09-02 10:37:35 -0700

    add solana web3 dep and some todos

---
+++ dashboard/public/pages/dashboard/psyoptions.ts
@@ -0,0 +1,5 @@
+import { Connection } from "@solana/web3.js";
+// TODO: Connect with Solana and load all underlying and quote asset pool accounts
+// TODO: Correlate the pool mints with the real world assets
+// TODO: Pull the asset prices for the assets and calculate TVL
+// const connection = new Connection("https://")
